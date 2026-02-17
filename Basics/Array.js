b73b93a6a2614847f82adcf61025515a1308bfdc
SREEJANII <bhattacharyasreejani@gmail.com>
2026-02-17 12:57:03 +0530

    Edited Basics

--- Basics/Array.js
+++
@@ -1,8 +0,0 @@
-function avg(...nums){
-    let sum=0;
-    for(let num of nums){
-        sum+=num;
-    }
-    return sum/nums.length;
-}
-console.log(avg(1,2,3,4,5));
--- Basics/Promt.js
+++
@@ -1,5 +0,0 @@
-let username=window.prompt("Please enter your name:");
-while(username==="" || username===null){
-    username=window.prompt("Please enter your name:");
-}
-console.log("Hello "+username);
