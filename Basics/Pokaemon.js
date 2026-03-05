befa548e0105709bb80d28cbc0ec1720392aa8c2
SREEJANII <bhattacharyasreejani@gmail.com>
2026-03-05 18:39:55 +0530

    IMproved the design of the Pokaemon website by adding new styles and enhancing the user interface. Updated the HTML structure to better organize content and improve accessibility. Refactored JavaScript code to optimize performance and add new features for a more interactive experience.

--- Basics/Pokaemon.js
+++ Basics/Pokaemon.js
@@ -1,12 +1,20 @@
 let name=document.getElementById("name");
 let bttn=document.getElementById("bttn");
-let img=document.getElementById("img");
-
+let container=document.getElementsByClassName("container")[0];
+let img=document.createElement("img");
 bttn.addEventListener("click",async ()=>{
+    try{
     let pokeName=name.value.toLowerCase();
     let url=`https://pokeapi.co/api/v2/pokemon/${pokeName}`;
     const data=await fetch(url);
     const jsonData=await data.json();
     console.log(jsonData);
     img.src=jsonData.sprites.other["official-artwork"].front_default;
+    img.alt=`Image of ${pokeName}`;
+    container.appendChild(img);
+    }
+    catch(error){
+        console.error("Error fetching data:",error);
+        alert("Pokemon not found. Please check the name and try again.");
+    }
 });
