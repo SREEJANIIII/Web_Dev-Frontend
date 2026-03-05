7b92cb603757a3ada3a1520f6d49f698a574e294
SREEJANII <bhattacharyasreejani@gmail.com>
2026-03-05 22:04:46 +0530

    Added extra features to the pokedex webpage. Now it also shows the type of the pokemon along with its image. The type is displayed in uppercase for better visibility.

--- Pokaemon/Pokaemon.js
+++ Pokaemon/Pokaemon.js
@@ -2,15 +2,20 @@ let name=document.getElementById("name");
 let bttn=document.getElementById("bttn");
 let container=document.getElementsByClassName("container")[0];
 let img=document.createElement("img");
+let type=document.createElement("p");
+type.classList.add("container");
 bttn.addEventListener("click",async ()=>{
-    try{
+    try{     
     let pokeName=name.value.toLowerCase();
     let url=`https://pokeapi.co/api/v2/pokemon/${pokeName}`;
-    const data=await fetch(url);
-    const jsonData=await data.json();
-    console.log(jsonData);
+    let data=await fetch(url);
+    jsonData=await data.json();
+  type.textContent=`Type: 1. ${jsonData.types[0].type.name} 2. ${jsonData.types[1] ? jsonData.types[1].type.name : "N/A"}`; // Check if second type exists
+  type.textContent=type.textContent.toUpperCase();   
+  console.log(jsonData);
     img.src=jsonData.sprites.other["official-artwork"].front_default;
     img.alt=`Image of ${pokeName}`;
+    container.appendChild(type);
     container.appendChild(img);
     }
     catch(error){
